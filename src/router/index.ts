59f6308374d6a0f965d850bcefdaa790bd66cdc7
RaulIto-senar <raul.ferreira@senarmt.org.br>
2025-09-26 14:33:15 -0500

    .1

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+//import HomeView from '../views/HomeView.vue'
+import QuestionView from '../views//question/QuestionView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    /*
     {
       path: '/',
       name: 'home',
@@ -12,11 +14,14 @@ const router = createRouter({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
     },
+    */
+    {
+      path: '/questions',
+      name: 'questions',
+      component: QuestionView,
+    }
   ],
 })
 
---
+++ src/views/question/component/header.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="flex justify-between items-center mb-6">
+        <h1 class="text-3xl sm:text-4xl font-black text-purple-600 text-center w-full">
+          <slot name="title"/>
+        </h1>
+        <button @click="returnTitle()">Alterar questões</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HeaderComponent',
+  props: {
+    title: {
+      type: String,
+      required: true,
+      default: 'Quiz2'
+    }
+  },
+  methods: {
+    returnTitle() {
+      this.$emit('returnTitle', 'Quiz')
+    }
+  }
+}
+</script>
+
+<style>
+</style>
+
